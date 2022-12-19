8adc063b5ffeadeed2c03dcd8f65ccadb0fc91ec
enemyOfProgress <vorobiov.bohdan@gmail.com>
2022-12-19 16:35:45 +0200

    add new test that check that user see login link, also add new function that open browser by the link for all tests.

--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,19 @@
 from .pages.main_page import MainPage
 
+
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
+    page = open_browser(browser, link)
+    page.got_to_login_page()
+
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = open_browser(browser, link)
+    page.should_be_login_link()
+
+
+def open_browser(browser, link):
     page = MainPage(browser, link)
     page.open()
-    page.got_to_login_page()
+    return page
