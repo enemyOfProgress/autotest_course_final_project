b011c12fce43c9aa1374e0a9e871905ddd9e4c3f
enemyOfProgress <vorobiov.bohdan@gmail.com>
2022-12-20 09:03:33 +0200

    fix syntax error got_to_login_page >> go_to_login_page

--- test_main_page.py
+++ test_main_page.py
@@ -3,7 +3,7 @@ from .pages.main_page import MainPage
 
 def test_guest_can_go_to_login_page(browser):
     page = open_browser(browser)
-    page.got_to_login_page()
+    page.go_to_login_page()
 
 
 def test_guest_should_see_login_link(browser):
