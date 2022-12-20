c044793f8825ac695763dd7da9aa88316bf7ad74
enemyOfProgress <vorobiov.bohdan@gmail.com>
2022-12-20 09:01:16 +0200

    Moved the link that opens the browser to the method so that it is not duplicated in the code

--- test_main_page.py
+++ test_main_page.py
@@ -2,18 +2,17 @@ from .pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = open_browser(browser, link)
+    page = open_browser(browser)
     page.got_to_login_page()
 
 
 def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = open_browser(browser, link)
+    page = open_browser(browser)
     page.should_be_login_link()
 
 
-def open_browser(browser, link):
+def open_browser(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
     page = MainPage(browser, link)
     page.open()
     return page
