a3c8416a2fa0e13974b428ee418f17a396a5c32f
enemyOfProgress <vorobiov.bohdan@gmail.com>
2022-12-21 19:18:15 +0200

    add class for login page and fill it by locators from page for login form and registration form

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,16 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#registration_link")
+
+
+class LoginPageLocators:
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    LOGIN_EMAIL_ADDRESS_FIELD = (By.CSS_SELECTOR, "#id_login-username")
+    LOGIN_PASSWORD_FIELD = (By.CSS_SELECTOR, "#id_login-password")
+    LOGIN_BUTTON = (By.CLASS_NAME, "btn.btn-lg.btn-primary")
+
+    REGISTRATION_FORM = (By.CSS_SELECTOR, "#register_form")
+    REGISTRATION_EMAIL_ADDRESS_FIELD = (By.CSS_SELECTOR, "#id_registration-email")
+    REGISTRATION_PASSWORD_FIELD = (By.CSS_SELECTOR, "#id_registration-password1")
+    REGISTRATION_CONFIRM_PASSWORD_FIELD = (By.CSS_SELECTOR, "#id_registration-password2")
+    REGISTRATION_BUTTON = (By.CLASS_NAME, "btn.btn-lg.btn-primary")
