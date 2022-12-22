6c6c29a97d403ea15af1bfa0dea819b884836536
enemyOfProgress <vorobiov.bohdan@gmail.com>
2022-12-22 09:58:33 +0200

    update func test_gues_can_go_to_login_page, that now this func check if the user on login page

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,12 @@
+import time
+
 from .pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
     page = open_browser(browser)
-    page.go_to_login_page()
+    login_page = page.go_to_login_page()
+    login_page.should_be_login_page()
 
 
 def test_guest_should_see_login_link(browser):
