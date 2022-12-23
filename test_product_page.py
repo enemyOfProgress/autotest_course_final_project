f641bcbccf0579b0392714c4a7e2baedd2527626
enemyOfProgress <vorobiov.bohdan@gmail.com>
2022-12-23 23:02:26 +0200

    add test that check that the user can see login link and can click on it

--- test_product_page.py
+++ test_product_page.py
@@ -7,7 +7,7 @@ from .pages.locators import ProductPageLocators
 # @pytest.mark.parametrize('offer_number', ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"])
 @pytest.mark.xfail
 def test_guest_can_add_product_to_basket(browser):
-    browser = open_basket_page(browser)
+    browser = open_product_page(browser)
     browser.add_product_to_basket()
     browser.solve_quiz_and_get_code()
 
@@ -23,26 +23,36 @@ def test_guest_can_add_product_to_basket(browser):
 
 
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
-    browser = open_basket_page(browser)
+    browser = open_product_page(browser)
     browser.add_product_to_basket()
     is_message_visible = browser.is_not_element_presented(*ProductPageLocators.MESSAGE_PRODUCT_NAME_IN_CART)
     assert is_message_visible, "The message should be, but test is ok"
 
 
 def test_guest_cant_see_success_message(browser):
-    browser = open_basket_page(browser)
+    browser = open_product_page(browser)
     is_message_visible = browser.is_not_element_presented(*ProductPageLocators.MESSAGE_PRODUCT_NAME_IN_CART)
     assert is_message_visible, "The message shouldn't be"
 
 
 def test_message_disappeared_after_adding_product_to_basket(browser):
-    browser = open_basket_page(browser)
+    browser = open_product_page(browser)
     browser.add_product_to_basket()
     message_is_disappear = browser.is_disappeared(*ProductPageLocators.MESSAGE_PRODUCT_NAME_IN_CART)
     assert message_is_disappear, "The message is still here"
 
 
-def open_basket_page(browser):
+def test_guest_should_see_login_link_on_product_page(browser):
+    browser = open_product_page(browser)
+    browser.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    browser = open_product_page(browser)
+    browser.go_to_login_page()
+
+
+def open_product_page(browser):
     link = f"http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
     browser = ProductPage(browser, link)
     browser.open()
