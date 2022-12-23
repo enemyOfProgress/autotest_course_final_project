b911f36ac7e2e7e8b11f625c5a4b15d654d2faf6
enemyOfProgress <vorobiov.bohdan@gmail.com>
2022-12-23 11:13:43 +0200

    create product page locators class and fill it by the locators for: messages with product name and amount in basket, and product name and amount from product

--- pages/locators.py
+++ pages/locators.py
@@ -20,3 +20,9 @@ class LoginPageLocators:
 
 class ProductPageLocators:
     ADD_TO_BASKET_BUTTON = (By.CLASS_NAME, "btn.btn-lg.btn-primary.btn-add-to-basket")
+    MESSAGE_PRODUCT_NAME_IN_CART = (By.CSS_SELECTOR, "#messages > div:nth-child(1) > div > strong")
+    MESSAGE_WITH_AMOUNT = (By.CSS_SELECTOR, "#messages > div.alert.alert-safe.alert-noicon.alert-info.fade.in > div > "
+                                            "p:nth-child(1)")
+    PRODUCT_NAME_FROM_PRODUCT = (By.CSS_SELECTOR, "#content_inner > article > div.row > div.col-sm-6.product_main > h1")
+    PRICE_FROM_PRODUCT = (By.CSS_SELECTOR, "#content_inner > article > div.row > div.col-sm-6.product_main > "
+                                           "p.price_color")
