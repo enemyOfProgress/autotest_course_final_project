a497204a1c3d7cc625b7dcb0079ec3afbff154f8
enemyOfProgress <vorobiov.bohdan@gmail.com>
2022-12-23 11:44:28 +0200

    add parametrize for link, so now test open 9 link one by one and change offer number in link, if test failed the test mark as xfail

--- test_product_page.py
+++ test_product_page.py
@@ -1,9 +1,13 @@
+import pytest
+
 from .pages.product_page import ProductPage
 from .pages.locators import ProductPageLocators
 
 
-def test_guest_can_add_product_to_basket(browser):
-    browser = open_basket_page(browser)
+@pytest.mark.parametrize('offer_number', ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"])
+@pytest.mark.xfail
+def test_guest_can_add_product_to_basket(browser, offer_number):
+    browser = open_basket_page(browser, offer_number)
     browser.add_product_to_basket()
     browser.solve_quiz_and_get_code()
 
@@ -18,9 +22,8 @@ def test_guest_can_add_product_to_basket(browser):
     browser.compare_the_amount(amount)
 
 
-def open_basket_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+def open_basket_page(browser, offer_number):
+    link = f"http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/?promo=offer{offer_number}"
     browser = ProductPage(browser, link)
     browser.open()
     return browser
-
