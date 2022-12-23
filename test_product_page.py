7e5062685c732a0a92aa79fada3b489418a6aa52
enemyOfProgress <vorobiov.bohdan@gmail.com>
2022-12-23 11:21:48 +0200

    create test file for check that the user can add product in basket and can see message about the product in cart

--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,7 @@
 import time
 
 from .pages.product_page import ProductPage
+from .pages.locators import ProductPageLocators
 
 
 def test_guest_can_add_product_to_basket(browser):
@@ -8,9 +9,21 @@ def test_guest_can_add_product_to_basket(browser):
     browser.add_product_to_basket()
     browser.solve_quiz_and_get_code()
 
+    # Check that the message with product name is visible, after user add product in cart
+    product = browser.is_message_visible(*ProductPageLocators.MESSAGE_PRODUCT_NAME_IN_CART)
+    # Compare product from cart and product from product page
+    browser.compare_the_product(product)
+
+    # Check that the message with amount is visible, after user add product in cart
+    amount = browser.is_message_visible(*ProductPageLocators.MESSAGE_WITH_AMOUNT)
+    # Compare amount in cart and product amount
+    browser.compare_the_amount(amount)
+    time.sleep(0)
+
 
 def open_basket_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     browser = ProductPage(browser, link)
     browser.open()
     return browser
+
