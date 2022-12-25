c05ec3e869d02c299db453d4caf300c0811ca710
enemyOfProgress <vorobiov.bohdan@gmail.com>
2022-12-25 14:18:36 +0200

    Add fun that checks, that the user cant see product in cart, if open basket page from main page

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,8 @@
+import time
+
 from .pages.main_page import MainPage
+from .pages.locators import BasketPageLocators
+from .pages.basket_page import BasketPage
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -12,8 +16,16 @@ def test_guest_should_see_login_link(browser):
     page.should_be_login_link()
 
 
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    browser = open_browser(browser)
+    browser.go_to_the_basket()
+    new_link = browser.get_current_url()
+    page = BasketPage(browser, new_link)
+    page.browser.is_basket_empty()
+
+
 def open_browser(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
+    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     return page
